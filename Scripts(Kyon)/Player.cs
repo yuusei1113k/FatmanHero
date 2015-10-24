5c3f7a548a2f42cd6f4fc85764ff76c713c93e00
saihe <saihe.kyon@gmail.com>
2015-10-24 18:07:58 +0900

    コントローラー修正。 タップでジャンプ。 フリックで瞬間移動。 タッチ距離制限(ベータ版)

--- Scripts(Kyon)/Player.cs
+++ Scripts(Kyon)/Player.cs
@@ -30,7 +30,8 @@ public class Player : MonoBehaviour {
     //タッチした位置と移動した位置の差分ベクトル
     private Vector3 direction;
 
-    private Vector3 flickJump;
+    //タップ判定かどうか
+    private bool tapOk;
 
     //directionに入れる座標
     private double x;
@@ -43,13 +44,11 @@ public class Player : MonoBehaviour {
     //カメラ
     GameObject cam;
 
-	// Use this for initialization
 	void Start () {
         cam = GameObject.Find("Main Camera");
         print(cam);
 	}
 	
-	// Update is called once per frame
 	void Update () {
         move();
     }
@@ -64,6 +63,7 @@ public class Player : MonoBehaviour {
             touchTime = 0;
             //タッチされるたびにフリック判定を初期化
             flickOk = false;
+            tapOk = false;
         }
 
         //タッチされている間
@@ -89,6 +89,9 @@ public class Player : MonoBehaviour {
             {
                 //print("touchTime: " + touchTime);
                 print("touchTime is short");
+
+                //タップ判定
+                tapOk = true;
                 
                 //指移動量Mathf.Abs(float value)でvalueの絶対値を返す
                 if (Mathf.Abs((float)x) > flickJdg || Mathf.Abs((float)z) > flickJdg)
@@ -106,8 +109,12 @@ public class Player : MonoBehaviour {
             }
 
             //タッチ位置と移動位置が同じなら移動
-            else if (dragPoint != touch)
+            //elseif (dragPoint != touch)
+            //フリックでもタップでもなければ移動
+            else
             {
+                //タップ判定オフ
+                tapOk = false;
 
                 //入力ベクトルをQuaternionに変換
                 Quaternion to = Quaternion.LookRotation(direction);
@@ -128,6 +135,7 @@ public class Player : MonoBehaviour {
             }
         }
 
+        //フリックアクション
         if (flickOk == true)
         {
             if (Input.GetMouseButtonUp(0))
@@ -137,17 +145,21 @@ public class Player : MonoBehaviour {
                 //初期化しないとそのうちコケる
                 transform.rotation = Quaternion.LookRotation(new Vector3(0f, 0f, 0f));
 
-                //アクション
-                //3.0f経過するまで繰り返す
-                while (transform.position.y < 5.0f)
-                {
-                    print(transform.position);
-                    direction.y = 0.1f;
-                    //移動
-                    transform.Translate(direction * speed);
-                }
+                //瞬間移動
+                transform.Translate(direction * 5);
                 flickOk = false;
                 print(flickOk);
+            }
+        }
+
+        //タップアクション
+        if(tapOk == true)
+        {
+            if (Input.GetMouseButtonUp(0))
+            {
+                //ジャンプ
+                transform.Translate(0, 5f, 0);
+                print("Tap");
             }
         }
     }
