64c9a74eb5635e638a04526a8565d7d368304d60
saihe <saihe.kyon@gmail.com>
2015-10-26 11:18:07 +0900

    タップするとオブジェクトを表示、1.0f後に非表示のスクリプト追加。 Player.cs l.178 attack()

--- Scripts(Kyon)/Player.cs
+++ Scripts(Kyon)/Player.cs
@@ -3,6 +3,18 @@ using System.Collections;
 
 public class Player : MonoBehaviour {
 
+    //移動判定かどうか
+    private bool moveOk;
+
+    //タップ判定かどうか
+    private bool tapOk;
+ 
+    //フリック用フリックなのか判定
+    private bool flickOk;
+
+    //タップ時に呼び出すオブジェクト
+    public GameObject pa;
+
     //プレイヤーの移動が逆になってしまった時用
     public bool reverse = false;
 
@@ -24,14 +36,8 @@ public class Player : MonoBehaviour {
     //フリック用タッチしている時間
     private double touchTime;
 
-    //フリック用フリックなのか判定
-    private bool flickOk;
-
     //タッチした位置と移動した位置の差分ベクトル
     private Vector3 direction;
-
-    //タップ判定かどうか
-    private bool tapOk;
 
     //directionに入れる座標
     private double x;
@@ -41,12 +47,9 @@ public class Player : MonoBehaviour {
     //回転速度
     private float rotationSpeed = 10000.0f;
 
-    //カメラ
-    GameObject cam;
 
 	void Start () {
-        cam = GameObject.Find("Main Camera");
-        print(cam);
+        pa.SetActive(false);
 	}
 	
 	void Update () {
@@ -64,6 +67,7 @@ public class Player : MonoBehaviour {
             //タッチされるたびにフリック判定を初期化
             flickOk = false;
             tapOk = false;
+            moveOk = false;
         }
 
         //タッチされている間
@@ -96,23 +100,22 @@ public class Player : MonoBehaviour {
                 //指移動量Mathf.Abs(float value)でvalueの絶対値を返す
                 if (Mathf.Abs((float)x) > flickJdg || Mathf.Abs((float)z) > flickJdg)
                 {
+                    tapOk = false;
+                    moveOk = false;
                     print("Flick stanby OK");
                     //フリックであると判定する
                     flickOk = true;
                 }
-                /*//必要か微妙
-                else
-                {
-                    return;
-                }
-                */
             }
 
             //タッチ位置と移動位置が同じなら移動
-            //elseif (dragPoint != touch)
             //フリックでもタップでもなければ移動
-            else
+            //else
+            else if (dragPoint != touch)
             {
+                //移動判定オン
+                moveOk = true;
+
                 //タップ判定オフ
                 tapOk = false;
 
@@ -130,8 +133,15 @@ public class Player : MonoBehaviour {
                     moveTo = new Vector3(cm.x * -1, 0, cm.z * -1) / 100;
                 }
 
+                print(moveTo * speed);
+
                 //移動
                 transform.Translate(moveTo * speed);
+            }
+            else
+            {
+                flickOk = false;
+                tapOk = false;
             }
         }
 
@@ -155,12 +165,25 @@ public class Player : MonoBehaviour {
         //タップアクション
         if(tapOk == true)
         {
-            if (Input.GetMouseButtonUp(0))
+            if (Input.GetMouseButtonDown(0))
             {
-                //ジャンプ
-                transform.Translate(0, 5f, 0);
                 print("Tap");
+                StartCoroutine(attack());
+                tapOk = false;
+                print(tapOk);
             }
+        }
+    }
+
+    IEnumerator attack()
+    {
+        pa.SetActive(true);
+        while (true)
+        {
+            yield return new WaitForSeconds(1.0f);
+            pa.SetActive(false);
+            print("hoge");
+            yield break;
         }
     }
 }
