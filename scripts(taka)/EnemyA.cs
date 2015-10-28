abdb2aae6192133ed3e7961ff1da4ba3ebbfdf55
takahashiN <ntaka0323@gmail.com>
2015-10-28 12:29:18 +0900

    敵が追いかけまわして攻撃モーションにはいる。接近すると球が飛び出すだけ

--- scripts(taka)/EnemyA.cs
+++ scripts(taka)/EnemyA.cs
@@ -12,9 +12,12 @@ public class EnemyA : MonoBehaviour {
 	private string[] state = new string[2] {"MOVE", "ATTACK"};
 	private float lastAttackTime;
 	private float attackInterval = 2f;
-	public float limitDistance = 5f;
+	public float limitDistance = 10f;
 	public Transform muzzle;
-	public GameObject bulletPrefab;
+	public GameObject bullet;
+	public Vector3 playerPos ;
+	public float distance;
+	public float waitForSeconds;
 
 
 
@@ -26,6 +29,7 @@ public class EnemyA : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
+
 
 
 		switch (enemyState){
@@ -41,34 +45,60 @@ public class EnemyA : MonoBehaviour {
 
 		
 	public void move(){
-		Vector3 playerPos = player.position; //プレーヤーの位置
+		playerPos = player.position; //プレーヤーの位置
+		distance = Vector3.Distance(playerPos , transform.position); 
 		Vector3 direction = playerPos - transform.position; //方向
 		direction = direction.normalized; //単位化（距離要素を取り除く
 		transform.position = transform.position + (direction * speed * Time.deltaTime);
+		print (direction);//方向
 		transform.LookAt (player);//プレーヤーの方を向く
+		if (distance <= limitDistance) {
+
+			enemyState = state [1];
+		}
 	}
 
 
 	public void attack(){
-		Vector3 playerPos = player.position;                 //プレイヤーの位置
-		Vector3 direction = playerPos - transform.position; //方向と距離を求める。
-		float distance = direction.sqrMagnitude;            //directionから距離要素だけを取り出す。
-		direction = direction.normalized;   
+		/*if (distance > limitDistance) {
+			print("Hoge");
+			enemyState = state [0];
+		}*/
 
-		if (Time.time > lastAttackTime + attackInterval) {
-			Instantiate(bulletPrefab, muzzle.position, muzzle.rotation);
-			lastAttackTime = Time.time;
+		playerPos = player.position;                 //プレイヤーの位置
 
-			transform.LookAt (player);
-		}
-		
+		distance = Vector3.Distance(playerPos , transform.position); //方向
+		StartCoroutine (attackstop ());
+
+	}	
 
 
+
+
+
+	IEnumerator attackstop(){
+		/*Vector3 nowPosition = new Vector3 (transform.position.x, 5f, transform.position.z);
+		transform.position = nowPosition;*/
+		//Instantiate(bullet, bullet.transform.position, bullet.transform.rotation);
+		bullet.SetActive (true);
+		lastAttackTime = Time.time;
+		//transform.position = transform.position;
+		yield return new WaitForSeconds (waitForSeconds);
+		enemyState = state [0];
+		bullet.SetActive (false);
+		yield break;
 	}
 	
-	
-	
+
+
+
 	/*void OnTriggerEnter(Collider coll) {
+		hp = hp - attackpower;
+		
+		if( hp <= 0){
+			Destory(GameObject);
+		}
+
 		if (coll.gameObject.tag == "Player") {
 			//Instantiate(Explosion, new Vector3(transform.position.x, transform.position.y, transform.position.z), Quaternion.identity);
 			Destroy(this.gameObject);
