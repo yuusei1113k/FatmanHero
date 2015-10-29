47b22e32ef61f4117fbe6affb662720cb2429465
mottan <a16.hatano@gmail.com>
2015-10-29 14:38:42 +0900

    これをBossにあたっちしてください。

---
+++ Script(Mottan)/Boss.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Boss : MonoBehaviour {
+	//public bool BD = GameObject.FindGameObjectWithTag("Boss");
+	// Use this for initialization
+	private SceneChanger SC;
+	void Start () {
+		
+	}
+	void Update () {
+		SC = FindObjectOfType<SceneChanger> ();
+	}
+	void OnCollisionEnter (Collision collision ) {
+		if (collision.gameObject.tag == "Player") {
+			SC.toResult ();
+		}
+	}
+}
