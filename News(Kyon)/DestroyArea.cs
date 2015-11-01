1d83f27ea8d4c44b45c735a73fe7de9d194712a4
yuusei1113k <yuusei1113k@gmail.com>
2015-11-01 19:59:29 +0900

    aaa

---
+++ News(Kyon)/DestroyArea.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DestroyArea : MonoBehaviour {
+
+    void OnCollisionEnter(Collision c)
+    {
+        c.gameObject.transform.position = new Vector3(0, 0, 0);
+    }
+
+    void OnTriggerEnter(Collider c)
+    {
+        c.gameObject.transform.position = new Vector3(0, 0, 0);
+    }
+}
