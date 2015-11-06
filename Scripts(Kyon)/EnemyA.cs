edcb9a36bc4f7a8269af33ded2148854f47086ee
saihe <saihe.kyon@gmail.com>
2015-11-06 14:21:43 +0900

    更新。

--- Scripts(Kyon)/EnemyA.cs
+++ Scripts(Kyon)/EnemyA.cs
@@ -35,6 +35,15 @@ public class EnemyA : MonoBehaviour {
     public AudioClip[] audioSorce;
     private AudioSource audio;
 
+    //コライダー
+    BoxCollider bcol;
+
+    //アニメーター
+    Animator anim;
+
+    //波動
+    public GameObject hado;
+
     void Start()
     {
         //プレイヤー取得
@@ -47,39 +56,41 @@ public class EnemyA : MonoBehaviour {
         //ステージマネージャーコンポーネント
         sm = FindObjectOfType<StageManager>();
 
-        //弾ポジション
-        bullet.transform.position = new Vector3(transform.position.x, 2, 10);
-        bullet.SetActive(false);
-
         //音を鳴らすコンポーネント
         audio = GetComponent<AudioSource>();
+
+        //コライダー
+        bcol = gameObject.GetComponent<BoxCollider>();
+
+        //アニメーション
+        anim = GetComponent<Animator>();
+
+        anim.SetLayerWeight(1, 1f);
+
     }
 
     void Update()
     {
-        //print(evilPoint);
         //プレーヤーの位置
         playerPos = player.transform.position;
 
         //プレイヤーとの距離
         distance = Vector3.Distance(transform.position, playerPos);
+
+        //プレーヤーの方を向く
+        transform.LookAt(player.transform);
+
         //wonderモード
-        if(distance > limitDistanse && evilPoint >= 0)
+        if (distance > limitDistanse && evilPoint >= 0)
         {
             nowState = enemyState[0];
+            anim.SetBool("Move", true);
         }
         //attackモード
         else if(distance <= limitDistanse && evilPoint >= 0)
         {
             nowState = enemyState[1];
-            try
-            {
-                bullet.SetActive(true);
-            }
-            catch (Exception)
-            {
-                
-            }
+            anim.SetBool("Move", false);
         }
         else
         //悪意0でアイテムポップ
@@ -96,16 +107,23 @@ public class EnemyA : MonoBehaviour {
         {
             case "wonder":
                 wonder();
+                attackOk = false;
                 break;
             case "attack":
                 //attack();
-                StartCoroutine(shot());
+                if(attackOk == false)
+                {
+                    StartCoroutine(attack());
+                    attackOk = true;
+                }
                 break;
             case "explode":
+                bcol.enabled = false;
                 StartCoroutine(explode());
                 break;
         }
     }
+    private bool attackOk;
 
     //やられたらカウント。
     void OnDisable()
@@ -130,55 +148,23 @@ public class EnemyA : MonoBehaviour {
     }
 
     //攻撃モード
-    public GameObject bullet;
-    //2秒間弾を出す
-    IEnumerator shot()
+    //攻撃コルーチン
+    IEnumerator attack()
     {
-        try
+        while (true)
         {
-
-            //方向
-            Vector3 direction = playerPos - bullet.transform.position;
-
-            //単位化（距離要素を取り除く
-            direction = direction.normalized;
-
-            //プレイヤーに向かって移動
-            bullet.transform.position = bullet.transform.position + (direction * speed * Time.deltaTime);
-
-            //プレーヤーの方を向く
-            transform.LookAt(player.transform);
-
-        }
-        catch (Exception)
-        {
-            yield break;
+            yield return new WaitForSeconds(1.0f);
+            anim.SetTrigger("Attack");
+            hado.SetActive(true);
+            print("hoge");
+            if (nowState != "attack")
+            {
+                hado.SetActive(false);
+                yield break;
+            }
         }
     }
-    public void attack()
-    {
-        try
-        {
 
-            //方向
-            Vector3 direction = playerPos - bullet.transform.position;
-
-            //単位化（距離要素を取り除く
-            direction = direction.normalized;
-
-            //プレイヤーに向かって移動
-            bullet.transform.position = bullet.transform.position + (direction * speed * Time.deltaTime);
-
-            //プレーヤーの方を向く
-            transform.LookAt(player.transform);
-
-        }
-        catch (Exception)
-        {
-
-        }
-
-    }
 
     //悪意（体力）外用
     public float evilPoint = 25f;
@@ -201,6 +187,7 @@ public class EnemyA : MonoBehaviour {
             //音量調整
             audio.volume = 0.1f;
 
+            //ジャブとスマッシュの攻撃力取得
             jabAtk = con.getJabAtk();
             smashAtk = con.getSmashAtk();
             switch (c.tag)
@@ -232,7 +219,8 @@ public class EnemyA : MonoBehaviour {
     //アイテムドロップ
     void itemPop()
     {
-        Instantiate(item[itemTmp], transform.position, Quaternion.identity).name = itemTmp.ToString();
+        Vector3 itemPos = new Vector3(transform.position.x, 1f, transform.position.z);
+        Instantiate(item[itemTmp], itemPos, Quaternion.identity).name = itemTmp.ToString();
     }
 
     //アイテムの抽選メソッド
@@ -247,11 +235,16 @@ public class EnemyA : MonoBehaviour {
     {
         // ランダムな吹き飛ぶ力を加える
         Vector3 force = Vector3.up * 1000f + UnityEngine.Random.insideUnitSphere * 300f;
-        GetComponent<Rigidbody>().AddForce(force);
+        //GetComponent<Rigidbody>().AddForce(force);
+        Vector3 up = new Vector3(0, 1f, 0);
+        transform.Translate(up * Time.deltaTime * 100, Space.World);
 
         // ランダムに吹き飛ぶ回転力を加える
         Vector3 torque = new Vector3(UnityEngine.Random.Range(-10000f, 10000f), UnityEngine.Random.Range(-10000f, 10000f), UnityEngine.Random.Range(-10000f, 10000f));
         GetComponent<Rigidbody>().AddTorque(torque);
+
+        //回転する
+        transform.Rotate(force);
 
         // 1秒後に自身を消去する
         yield return new WaitForSeconds(3.0f);
