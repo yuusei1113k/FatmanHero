d1fb9329f2b812c976d180c9f4aae893eecfb2c4
saihe <saihe.kyon@gmail.com>
2015-11-10 14:32:48 +0900

    悪意バーと残像追加。

---
+++ News(Kyon)/Controller.cs
@@ -0,0 +1,396 @@
+﻿using UnityEngine;
+using System.Collections;
+using GameSystems;
+using System.Collections.Generic;
+using System.Linq;
+
+public class Controller : MonoBehaviour {
+
+    //移動判定かどうか
+    private bool moveOk = false;
+	
+	//タップ判定かどうか
+	private bool tapOk = false;
+	
+	//フリック用フリックなのか判定
+	private bool flickOk = false;
+
+	//プレイヤーの移動が逆になってしまった時用
+	public bool reverse = false;
+	
+	//プレイヤーの移動スピード調整用変数
+	public float speed = 1;
+	
+	//タッチされた座標
+	private Vector2 touch;
+	
+	//移動先のワールド座標
+	private Vector3 cm;
+	private Vector3 moveTo;
+	
+	//フリック判定用タッチ判定時間
+	private double touchJdg = 0.15;
+	
+	//フリック判定用タッチ判定移動量
+	private double flickJdg = 30;
+	
+	//タッチ後移動した座標
+	private Vector2 dragPoint;
+	
+	//フリック用タッチしている時間
+	private double touchTime;
+	
+	//タッチした位置と移動した位置の差分ベクトル
+	private Vector3 direction;
+	
+	//directionに入れる座標
+	private double x;
+	private double y;
+	private double z;
+	
+	//回転速度
+	private float rotationSpeed = 10000.0f;
+	
+    //Buttonコンポーネント
+	Buttons button;
+
+    //アニメーション
+    Animator anim;
+
+    //Stateクラス
+    State state = new State();
+
+    //更生力
+    private float attack;
+
+    //アクションカウント
+    private int tapCount = 0;
+
+    //オーディオソース
+    private string[] audioList = new string[3] {"punch-swing", "jabpunch", "itemget"}; 
+    private AudioClip[] audioSorce = new AudioClip[3];
+    private AudioSource audio;
+
+    //波動
+    private GameObject hado;
+
+    //BMIManager
+    private BMIManager bmiManager;
+
+    //攻撃判定オンオフ用コライダー
+    SphereCollider jab;
+    SphereCollider smash;
+
+    //残像
+    TrailRenderer trail;
+
+
+
+    void Start () {
+        //BMIManager
+        bmiManager = GameObject.Find("BMIManager").GetComponent<BMIManager>();
+		//攻撃判定オフ
+		button = GameObject.Find("Screen").GetComponent<Buttons>();
+
+        //モーションをいじるため
+        anim = GetComponent<Animator>();
+
+        //オーディオソースコンポーネント
+        audio = GetComponent<AudioSource>();
+        //オーディオクリップをリソースフォルダから取得
+        for(int i = 0; i < audioSorce.Length; i++)
+        {
+            audioSorce[i] = (AudioClip)Resources.Load("SEfects/" + audioList[i]);
+        }
+
+        //波動非表示
+        hado = transform.GetChild(3).gameObject;
+        hado.SetActive(false);
+
+        //攻撃判定用
+        jab = GameObject.FindWithTag("Jab").GetComponent<SphereCollider>();
+        smash = GameObject.FindWithTag("Smash").GetComponent<SphereCollider>();
+        jab.enabled = false;
+        smash.enabled = false;
+
+        //残像
+        trail = transform.GetComponent<TrailRenderer>();
+        trail.enabled = false;
+    }
+
+    void Update () {
+		if (state.getState() != GameState.Pausing)
+		{
+            move();
+		}
+	}
+	
+	//コントローラー状態
+	public bool getFlick()
+	{
+		return flickOk;
+	}
+	
+	//コントローラー本体
+	public void move()
+	{
+		//タッチされた瞬間のみ
+		if (Input.GetMouseButtonDown(0))
+		{
+			//タッチされた座標を取得
+			touch = new Vector2(Input.mousePosition.x, Input.mousePosition.y);
+			touchTime = 0;
+			//タッチされるたびにフリック判定を初期化
+			flickOk = false;
+			tapOk = false;
+			moveOk = false;
+		}
+		if (button.getPushButton() == false && state.getState() == GameState.Playing)
+		{
+			//タッチされている間
+			if (Input.GetMouseButton(0))
+			{
+				//タップ判定
+				tapOk = false;
+				
+				//タッチ後移動した座標
+				dragPoint = new Vector2(Input.mousePosition.x, Input.mousePosition.y);
+				
+				//プレイヤーが移動するベクトル
+				x = dragPoint.x - touch.x;
+				y = 0;
+				z = dragPoint.y - touch.y;
+				
+				//タッチされてる時間を計測
+				touchTime += Time.deltaTime;
+				
+				//入力をVector3に変換/移動量を制限
+				direction = new Vector3((float)x, (float)y, (float)z) / 1000;
+				
+				//フリック判定用
+				Vector3 pointA = new Vector3(touch.x, 0, touch.y);
+				Vector3 pointB = new Vector3(Mathf.Clamp(dragPoint.x, touch.x - 60, touch.x + 60), 0, Mathf.Clamp(dragPoint.y, touch.y - 60, touch.y + 60));
+				//二点間の距離(float)
+				float flickVector = Vector3.Distance(pointA, pointB);
+				
+				//フリックスピード
+				double flickSpeed = flickVector / touchTime;
+				
+				//フリックスピードが800以上あればフリック
+				if (flickSpeed > 800)
+				{
+					//フリックであると判定する
+					flickOk = true;
+				}
+				
+				//タッチ位置と移動位置が同じなら移動
+				else if (dragPoint != touch)
+				{
+					//移動判定オン
+					moveOk = true;
+
+                    //移動モーション
+                    if(bmiManager.getSkillOn() == false)
+                    {
+                        anim.SetBool("Move", true);
+                        anim.SetTrigger("Move");
+                    }
+
+                    //入力ベクトルをQuaternionに変換
+                    Quaternion to = Quaternion.LookRotation(direction);
+					
+					//キャラクターを向かせる
+					transform.rotation = Quaternion.RotateTowards(transform.rotation, to, rotationSpeed * Time.deltaTime);
+
+                    //反転用
+                    if (reverse == true)
+					{
+						direction = new Vector3(-direction.x, 0, -direction.z);
+					}
+
+					//移動
+					transform.Translate(direction.normalized * 0.1f * speed, Space.World);
+				}
+				//移動でもフリックでもなければ
+				else if (touchTime < touchJdg)
+				{
+					flickOk = false;
+					moveOk = false;
+					tapOk = true;
+				}
+			}
+            if (Input.GetMouseButtonUp(0))
+            {
+                anim.SetBool("Move", false);
+            }
+			
+		}
+		
+		
+		//フリックアクション
+		if (flickOk == true)
+		{
+			if (Input.GetMouseButtonUp(0))
+			{
+				print("Flick");
+                anim.SetTrigger("Flick");
+				transform.rotation = Quaternion.RotateTowards(transform.rotation, Quaternion.LookRotation(direction), rotationSpeed * Time.deltaTime);
+				
+				//反転用
+				if (reverse == true)
+				{
+					direction = new Vector3(-direction.x, 0, direction.z);
+				}
+				transform.Translate(direction * 100, Space.World);
+                StartCoroutine(flick());
+				flickOk = false;
+			}
+		}
+
+        //タップアクション
+		if(tapOk == true)
+		{
+			if (Input.GetMouseButtonUp(0))
+			{
+				print("Tap");
+                tapCount++;
+                anim.SetBool("Move", false);
+                anim.SetTrigger("Attack");
+                StartCoroutine(Hado());
+                //攻撃モーション時に全身
+                if(tapCount / 3 != 1)
+                {
+                    jab.enabled = true;
+                    hado.tag = "Jab";
+                    transform.Translate(transform.forward * 2 * Time.deltaTime);
+                }
+                else
+                {
+                    smash.enabled = true;
+                    hado.tag = "Smash";
+                    transform.Translate(transform.forward / 10);
+                    tapCount = 0;
+                }
+                tapOk = false;
+                //jab.enabled = false;
+                //smash.enabled = false;
+            }
+        }
+    }
+
+    //フリックアクション
+    IEnumerator flick()
+    {
+        trail.enabled = true;
+        yield return new WaitForSeconds(0.5f);
+        trail.enabled = false;
+        yield break;
+    }
+
+    /*
+    探知機: Sphere Collider, Center(0, 1, 0), Radius(3), Is Trigger(On)
+    探知機に当たった物体を格納するコレクション
+    Key  : 接触GameObject 
+    Value: プレイヤーとの距離
+    */
+    public Dictionary<GameObject, float> list = new Dictionary<GameObject, float>();
+    void OnTriggerStay(Collider c)
+    {
+        float min = 10f;
+        //print("OnTri: " + c);
+        //Enemyタグがついたオブジェクトのみコレクションに格納
+        if (c.tag == "Enemy" || c.tag == "Boss")
+        {
+            if (list.ContainsKey(c.gameObject) == false)
+            {
+                //コレクションに存在しない場合追加
+                list.Add(c.gameObject, Vector3.Distance(transform.position, c.transform.position));
+            }
+            else
+            {
+                //既にコレクションに存在したらValueを更新
+                list[c.gameObject] = Vector3.Distance(transform.position, c.transform.position);
+            }
+
+            //コレクションの中で最も近いGameObjectに向く
+            foreach(var val in list)
+            {
+                min = val.Value;
+                //プレイヤーに近い方に向く
+                if (min >= val.Value)
+                {
+                    Transform target = val.Key.gameObject.transform;
+                    //print("target: " + target);
+                    transform.LookAt(target);
+                }
+            }
+        }
+
+        //敵の攻撃にあったたら
+        if (c.gameObject.tag == "Bullet")
+        {
+            print("HIt to Player: Bullet");
+            bmi -= 5f;
+            c.gameObject.SetActive(false);
+            //Destroy(c.gameObject);
+        }
+    }
+
+    //離れたらコレクションから削除
+    void OnTriggerExit(Collider c)
+    {
+        if (list.ContainsKey(c.gameObject))
+        {
+            list.Remove(c.gameObject);
+        }
+    }
+    //BMI外用
+    public float bmi = 200f;
+    //取得
+    public float getBMI()
+    {
+        return bmi;
+    }
+    //セット
+    public void setBMI(float f)
+    {
+        bmi = f;
+    }
+    //足す
+    public void incBMI(float f)
+    {
+        bmi += f;
+    }
+
+    //更生力 外用
+    private float jabAtk = 1f;
+    private float smashAtk = 3f;
+    //取得
+    public float getJabAtk()
+    {
+        return jabAtk;
+    }
+    public float getSmashAtk()
+    {
+        return smashAtk;
+    }
+    //セット
+    public void setJabAtk(float f)
+    {
+        jabAtk = f;
+    }
+    public void setSmashAtk(float f)
+    {
+        smashAtk = f;
+    }
+
+    //タップ時波動エフェクトを出す
+    IEnumerator Hado()
+    {
+        hado.SetActive(true);
+        yield return new WaitForSeconds(0.2f);
+        hado.SetActive(false);
+        yield break;
+    }
+
+}
---
+++ News(Kyon)/EnemyA.cs
@@ -0,0 +1,313 @@
+﻿using UnityEngine;
+using System.Collections;
+using GameSystems;
+using System;
+using UnityEngine.UI;
+
+public class EnemyA : MonoBehaviour
+{
+
+    //プレイヤー座標取得用
+    private GameObject player;
+    private Vector3 playerPos;
+
+    //調整用スピード
+    public float speed = 1;
+
+    //ステート
+    private string[] enemyState = new string[3] { "wonder", "attack", "explode" };
+    //ステート変更用
+    private string nowState;
+
+    //プレイヤーとの距離
+    private float limitDistanse = 3;
+    private float distance;
+
+    StageManager sm;
+
+    ScenChanger sc = new ScenChanger();
+
+    Controller con;
+
+    //アイテム関連
+    private GameObject[] item = new GameObject[5];
+    private int itemTmp;
+
+    //音
+    public AudioClip[] audioSorce;
+    private AudioSource audio;
+
+    //コライダー
+    BoxCollider bcol;
+
+    //アニメーター
+    Animator anim;
+
+    //波動
+    private GameObject hado;
+
+    //プレイヤーの更生力
+    private float jabAtk;
+    private float smashAtk;
+    private bool attackOk;
+    
+    //悪意
+    public float evilPoint = 25f;
+
+    //カメラオブジェクト
+    GameObject camera;
+
+    //悪意バー
+    GameObject bar;
+    Slider slider;
+
+
+    void Start()
+    {
+        //プレイヤー取得
+        player = GameObject.FindGameObjectWithTag("Player");
+        con = player.GetComponent<Controller>();
+
+        //波動
+        hado = transform.GetChild(4).gameObject;
+
+        //徘徊モードにする
+        nowState = enemyState[0];
+
+        //アイテムをリソースフォルダから取得
+        for(int i = 0; i < item.Length; i++)
+        {
+            item[i] = (GameObject)Resources.Load("Items/" + i);
+        }
+
+        //ステージマネージャーコンポーネント
+        sm = GameObject.Find("StageManager").GetComponent<StageManager>();
+
+        //音を鳴らすコンポーネント
+        audio = GetComponent<AudioSource>();
+
+        //コライダー
+        bcol = gameObject.GetComponent<BoxCollider>();
+
+        //アニメーション
+        anim = GetComponent<Animator>();
+        //アニメーターレイヤー指定
+        anim.SetLayerWeight(1, 1f);
+
+        //カメラ
+        camera = GameObject.Find("Camera");
+
+        //悪意バー
+        bar = transform.GetChild(5).gameObject;
+        slider = bar.GetComponent<Slider>();
+        slider.maxValue = evilPoint;
+
+    }
+
+    void Update()
+    {
+        //悪意バーがカメラを向く
+        bar.transform.LookAt(camera.transform);
+        slider.value = evilPoint;
+
+        //プレーヤーの位置
+        playerPos = player.transform.position;
+
+        //プレイヤーとの距離
+        distance = Vector3.Distance(transform.position, playerPos);
+
+        //プレーヤーの方を向く
+        transform.LookAt(player.transform);
+
+        //wonderモード
+        if (distance > limitDistanse && evilPoint >= 0)
+        {
+            nowState = enemyState[0];
+            anim.SetBool("Move", true);
+        }
+        //attackモード
+        else if (distance <= limitDistanse && evilPoint >= 0)
+        {
+            nowState = enemyState[1];
+            anim.SetBool("Move", false);
+        }
+        else
+        //悪意0でアイテムポップ
+        if (evilPoint <= 0)
+        {
+            //Controllerのターゲットリストから削除
+            con.list.Remove(gameObject);
+            nowState = enemyState[2];
+        }
+
+        //print(nowState);
+        //ステートによりモード切替
+        switch (nowState)
+        {
+            case "wonder":
+                wonder();
+                attackOk = false;
+                break;
+            case "attack":
+                //attack();
+                if (attackOk == false)
+                {
+                    StartCoroutine(attack());
+                    attackOk = true;
+                }
+                break;
+            case "explode":
+                bcol.enabled = false;
+                StartCoroutine(explode());
+                break;
+        }
+    }
+
+    //やられたらカウント。
+    void OnDisable()
+    {
+        try
+        {
+            sm.Counter(1);
+        }catch(Exception e)
+        {
+            print(e);
+        }
+    }
+
+    //徘徊モード
+    public void wonder()
+    {
+        //方向
+        Vector3 direction = playerPos - transform.position;
+
+        //単位化（距離要素を取り除く
+        direction = direction.normalized;
+
+        //プレイヤーに向かって移動
+        transform.position = transform.position + (direction * speed * Time.deltaTime);
+
+        //プレーヤーの方を向く
+        transform.LookAt(player.transform);
+    }
+
+    //攻撃モード
+    //攻撃コルーチン
+    IEnumerator attack()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(1.0f);
+            anim.SetTrigger("Attack");
+            hado.SetActive(true);
+            if (nowState != "attack")
+            {
+                hado.SetActive(false);
+                yield break;
+            }
+        }
+    }
+
+
+    //悪意（体力）外用
+    public float getEvil()
+    {
+        return evilPoint;
+    }
+    public void setEvil(float e)
+    {
+        evilPoint += e;
+    }
+
+    void OnTriggerEnter(Collider c)
+    {
+        try
+        {
+            //音量調整
+            audio.volume = 0.1f;
+
+            //ジャブとスマッシュの攻撃力取得
+            jabAtk = con.getJabAtk();
+            smashAtk = con.getSmashAtk();
+            switch (c.tag)
+            {
+                case "Jab":
+                    print("Hit to Enemy: Jab");
+                    //ジャブのヒット音
+                    audio.PlayOneShot(audioSorce[0]);
+                    evilPoint -= jabAtk;
+                    break;
+                case "Smash":
+                    print("Hit to Enemy: Smash");
+                    //スマッシュのヒット音
+                    audio.PlayOneShot(audioSorce[1]);
+                    evilPoint -= smashAtk;
+                    break;
+                case "Hado":
+                    print("Hit to Enemy: Hado");
+                    //スマッシュのヒット音
+                    audio.PlayOneShot(audioSorce[1]);
+                    evilPoint -= smashAtk;
+                    break;
+                case "Sonic":
+                    print("Hit to Enemy: Sonic");
+                    //ジャブのヒット音
+                    audio.PlayOneShot(audioSorce[0]);
+                    evilPoint -= jabAtk;
+                    break;
+                case "Rush":
+                    print("Hit to Enemy: Rush");
+                    //ジャブのヒット音
+                    audio.PlayOneShot(audioSorce[0]);
+                    evilPoint -= jabAtk * 2;
+                    break;
+                case "Havoc":
+                    print("Hit to Enemy: Havoc");
+                    //スマッシュのヒット音
+                    audio.PlayOneShot(audioSorce[1]);
+                    evilPoint -= smashAtk * 3;
+                    break;
+            }
+        }
+        catch (Exception) { }
+    }
+
+    //アイテムドロップ
+    void itemPop()
+    {
+        Vector3 itemPos = new Vector3(transform.position.x, -3f, transform.position.z);
+        Instantiate(item[itemTmp], itemPos, Quaternion.identity).name = itemTmp.ToString();
+    }
+
+    //アイテムの抽選メソッド
+    public int itemRnd()
+    {
+        itemTmp = UnityEngine.Random.Range(0, 3);
+        return itemTmp;
+    }
+
+    //吹き飛ぶ
+    IEnumerator explode()
+    {
+        // ランダムな吹き飛ぶ力を加える
+        Vector3 force = Vector3.up * 1000f + UnityEngine.Random.insideUnitSphere * 300f;
+        //GetComponent<Rigidbody>().AddForce(force);
+        Vector3 up = new Vector3(0, 1f, 0);
+        transform.Translate(up * Time.deltaTime * 15, Space.World);
+
+        // ランダムに吹き飛ぶ回転力を加える
+        Vector3 torque = new Vector3(UnityEngine.Random.Range(-10f, 10f), UnityEngine.Random.Range(-10f, 10f), UnityEngine.Random.Range(-10f, 10f));
+        GetComponent<Rigidbody>().AddTorque(torque);
+
+        //回転する
+        transform.Rotate(force);
+
+        // 1秒後に自身を消去する
+        yield return new WaitForSeconds(3.0f);
+        gameObject.SetActive(false);
+        //アイテム抽選、アイテムドロップ
+        itemRnd();
+        itemPop();
+
+    }
+}
